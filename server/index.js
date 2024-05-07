26411543efcddcab939b9698ceb0f85011628a53
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-05-06 21:28:15 -0500

    ADD connectionStateRecovery property in new Server for offline messages recovery

--- server/index.js
+++ server/index.js
@@ -1,8 +1,29 @@
 import express from 'express'
 import morgan from 'morgan'
-const PORT = process.env.PORT ?? 3000
 
+// Socket.io
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
+const PORT = process.env.PORT ?? 3000
 const app = express()
+const server = createServer(app)
+const io = new Server(server, {
+  // Para cuando se pierde la conexion
+  connectionStateRecovery: {}
+})
+
+io.on('connection', (socket) => {
+  console.log('A user has connected!!')
+
+  socket.on('disconnect', () => {
+    console.log('An usar has disconnected')
+  })
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
+  })
+})
 
 // Middlewares
 app.use(morgan('dev'))
@@ -11,6 +32,6 @@ app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server listening in port: http://localhost:${PORT}`)
 })
