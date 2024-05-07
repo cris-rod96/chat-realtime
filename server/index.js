8da7c307eb82c9a442688d4696dc950dccd69a98
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-05-06 19:14:21 -0500

    ADD Client html with basic style

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from 'express'
-
+import morgan from 'morgan'
 const PORT = process.env.PORT ?? 3000
 
 const app = express()
 
+// Middlewares
+app.use(morgan('dev'))
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world </h1>')
+  res.sendFile(process.cwd() + '/client/index.html')
 })
 
 app.listen(PORT, () => {
